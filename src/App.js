d4d92e6616debbbcc0c3e7f6b8e81d33c69decc6
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-02 18:55:55 +0100

    Set up containers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import {Article, Brand, Cta} from './components'
+
 
 const App = () => {
   return (
     <div>
       <h1>GPT-3</h1>
-      <Article />
-      <Brand />
-      <Cta />
-      
+            
     </div>
   )
 }
